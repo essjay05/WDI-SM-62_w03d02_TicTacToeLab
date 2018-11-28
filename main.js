26c4d361e67cfe68ac0b8ef3772ed5819647ec40
Joy <stepjoyous@gmail.com>
2018-11-27 16:13:11 -0800

    Establish grids in CSS

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 console.log('JS Loaded');
 
-prompt('Click to begin game');
+
+var pX = 'playerX';
+var pO = 'playerO';
+
+
+var cells = document.querySelector('.cell');
+var cell = cells.children;
+cells.addEventListener('click', function (go) {
+    cell
+})
+
+
+function currentPlayer(player) {
+    if 
+}
